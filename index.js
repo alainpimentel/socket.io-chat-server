3e885663c03afdf38b7536224285e94d97881dea
hpimentel <hpimentel@southern.edu>
2014-08-08 12:42:02 -0400

    Socket disconnected:

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile('index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
